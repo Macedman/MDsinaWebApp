5c696a87d1a3cd753fdf0a8aee85448460355ae8
Ec da KreatOr <88333318+Macedman@users.noreply.github.com>
2022-01-05 22:35:13 +0800

    Added Dashboard

--- index.php
+++ index.php
@@ -17,28 +17,29 @@
 </head>
 
     <!--Log in-->
-  <body class="text-center">
-    <form class="form-signin">
+    <form method="POST" action="#" class="form-signin">
+  <body class="text-center" style="background-color: #FEF5ED;">
       <img class="mb-4" src="assets/logo.png" alt="Logo" width="72" height="90">
       <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+        <!--Email-->
       <label for="inputEmail" class="sr-only">Email address</label>
       <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+        <!--Password-->
       <label for="inputPassword" class="sr-only">Password</label>
-      <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-      
-      <div class="checkbox mb-3">
-      <div class='login_fields__password'>
+      <input type="password" id="inputPassword" class="form-control mt-2" placeholder="Password" required>
+      <!--Dropdown-->
+      <div class="mb-3 mt-3">
 	 <select name="ltype" required>
 	 <option value="">Select Role</option>
 	 <option value="Admin">Admin</option>
 	 <option value="Vendor">Vendor(Store)</option>
 	 <option value="D_boy">Delivery Manager</option>
 	 </select>
-	 </div>
-      </div>
-      <button class="btn btn-lg btn-primary btn-block" value="Log in" type="submit">Log in</button>
+      <!--Submit Button-->
+      <button class="btn btn-lg btn-primary btn-block mt-3" value="Log in" type="submit">Log in</button>
       <p class="mt-5 mb-3 text-muted">&copy; MDsina 2021-2022</p>
+      </div>
     </form>
-    </div>
+  
 </body>
 </html>
