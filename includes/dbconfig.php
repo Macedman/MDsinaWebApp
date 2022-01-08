e8253c704744cac6fab9bf16c6d769b9b787fc81
Ec da KreatOr <88333318+Macedman@users.noreply.github.com>
2022-01-08 19:48:03 +0800

    Fixed config

--- includes/dbconfig.php
+++ includes/dbconfig.php
@@ -1,14 +1,18 @@
 <?php
+
+if (session_status () == PHP_SESSION_NONE) {
+    session_start();
+}
     $servername = "localhost";
     $username = "root";
     $password = "";
     $database = "mdsina";
 
     //Create connection 
-    $conn = new mysqli($servername, $username, $password, $database);
+    $mysqli = new mysqli($servername, $username, $password, $database);
 
     //Check connection 
-    if ($conn->connect_error) {
+    if ($mysqli->connect_error) {
         die ("Connection failed" . $conn->connect_error);
     } 
     else {
