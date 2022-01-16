94a0ee7b0eaedc1d1b637bb690dd347d41478fc5
Ec da KreatOr <88333318+Macedman@users.noreply.github.com>
2022-01-16 23:09:24 +0800

    Added Features to Sidebar

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -12,16 +12,27 @@
 </head>
 <body>
 
-<body>
-   <div class="sidebar">
+<!--Sidebar Header-->
+<div class="sidebar">
      <div class="logo-details">
-      <i class='bx bxl-bootstrap'></i>
+      <i class='bx bxl-html5'></i> 
       <span class="logo_name">MDsina</span>
      </div>
-   </div>
+   
 
+   <!--Sidebar UL-->
+    <ul class="nav-links">
+      <li>
+        <a href="#">
+          <i class='bx bx-grid-alt' ></i>
+          <span class="link_name">Dashboard</span>
+        </a>
+        <ul class="sub-menu blank">
+          <li><a class="link_name" href="">Dashboard</a></li>
+        </ul>
+      </li>
 
-   <li>
+      <li>
         <div class="icon-link">
         <a href="#">
           <i class='bx bx-collection' ></i>
@@ -30,7 +41,9 @@
         <i class='bx bx-chevron-down arrow' ></i>
         </div>
         <ul class="sub-menu">
+          <li><a class="link_name" href="">Category</a></li>
           <li><a href="">Add Category</a></li>
+          <li><a href="">List Category</a></li>
           <li><a href="">List Category</a></li>
         </ul>
       </li>
@@ -44,9 +57,149 @@
         </a>
         <i class='bx bx-chevron-down arrow' ></i>
         </div>
+        <ul class="sub-menu">
+          <li><a class="link_name" href="">Pincode</a></li>
+          <li><a href="">Add Pincode</a></li>
+          <li><a href="">List Pincode</a></li>
+          <li><a href="">List Category</a></li>
+          <li><a href="">List Category</a></li>
+        </ul>
+      </li>
+
+      <li>
+        <a href="#">
+          <i class='bx bx-store-alt'></i>
+          <span class="link_name">Store</span>
+        </a>
+        <ul class="sub-menu blank">
+          <li><a class="link_name" href="">Store</a></li>
+        </ul>
+      </li>
+
+      <li>
+        <a href="#">
+          <i class='bx bx-package'></i>
+          <span class="link_name">Payment Gateway</span>
+        </a>
+        <ul class="sub-menu blank">
+          <li><a class="link_name" href="">List Payment Gateway</a></li>
+        </ul>
+      </li>
+
+      <li>
+        <div class="icon-link">
+        <a href="#">
+          <i class='bx bx-map-pin' ></i>
+          <span class="link_name">Rider</span>
+        </a>
+        <i class='bx bx-chevron-down arrow' ></i>
+        </div>
+        <ul class="sub-menu">
+          <li><a class="link_name" href="">Rider</a></li>
+          <li><a href="">Add Rider</a></li>
+          <li><a href="">List Rider</a></li>
+          <li><a href="">List Category</a></li>
+        </ul>
+      </li>
+
+      <li>
+        <a href="#">
+          <i class='bx bx-package'></i>
+          <span class="link_name">Customers</span>
+        </a>
+        <ul class="sub-menu blank">
+          <li><a class="link_name" href="">Customers</a></li>
+        </ul>
+      </li>
+
+      <li>
+        <div class="icon-link">
+        <a href="#">
+          <i class='bx bx-map-pin' ></i>
+          <span class="link_name">Testimonial</span>
+        </a>
+        <i class='bx bx-chevron-down arrow' ></i>
+        </div>
+        <ul class="sub-menu">
+          <li><a class="link_name" href="">Testemonial</a></li>
+          <li><a href="">Add Testemonial</a></li>
+          <li><a href="">List Testemonial</a></li>
+          <li><a href="">List Category</a></li>
+          <li><a href="">List Category</a></li>
+        </ul>
+      </li>
+
+      <li>
+        <a href="#">
+          <i class='bx bx-package'></i>
+          <span class="link_name">Order List</span>
+        </a>
+        <ul class="sub-menu blank">
+          <li><a class="link_name" href="">Order List</a></li>
+        </ul>
+      </li>
+
+      <li>
+        <a href="#">
+          <i class='bx bx-package'></i>
+          <span class="link_name">Update Profile</span>
+        </a>
+        <ul class="sub-menu blank">
+          <li><a class="link_name" href="">Update Profile</a></li>
+        </ul>
+      </li>
+      
+      <li>
+        <a href="#">
+          <i class='bx bx-package'></i>
+          <span class="link_name">Setting</span>
+        </a>
+        <ul class="sub-menu blank">
+          <li><a class="link_name" href="">Setting</a></li>
+        </ul>
       </li>
 
     
+      <li>
+      <div class="profile-details">
+        <div class="profile-content">
+          <img src="images/profile.jpg" alt="profile">
+        </div>
+        
+        <div class="name-job">
+          <div class="profile_name">Ec Macedaa</div>
+          <div class="job">Web Developer</div>
+        </div>
+        <i class='bx bx-log-out'></i>
+      </div>
+    </li>
+  </ul>
+  </div>
+
+  <section class="home-section">
+    <div class="home-content">
+      <i class='bx bx-menu'></i>
+      <span class="text">Drop Down Sidebar</span>
+    </div>
+  </section>
+
+<script>
+    let arrow = document.querySelectorAll(".arrow");
+    console.log(arrow);
+
+    for (var i = 0; i < arrow.length; i++) {
+      arrow[i].addEventListener("click", (e)=>{
+        let arrowParent = e.target.parentElement.parentElement;
+        
+        arrowParent.classList.toggle("showMenu");
+
+      });
+    }
+
+    let sidebar = document.querySelector(".sidebar");
+    console.log(sidebar);
+
+</script>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"></script>
